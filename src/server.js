42ff2c11cfe5943559668941515a0e9353c8d159
Carlos Grecco <carlosgrecco16@gmail.com>
2026-08-09 15:24:47 -0300

    primeiro comit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('API online');
+});
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server rodando na porta ${PORT}`);
+});
